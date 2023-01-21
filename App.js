de99ed5c4e3cb36c06fc80c8008e6856a0ec10b5
Ishan Sharma <ishansharma1320@gmail.com>
2023-01-21 19:06:46 -0400

    Lecture 17: Exploring Layouts

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}> Hi There</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+     <View>
+      <TextInput style={styles.goalInput} placeholder='Ex: My Goal is to ...'></TextInput>
+      <Button title='Submit Goal' />
+     </View>
+     <View>
+      <Text style={styles.text}> List of Goals .... </Text>
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer:{
+    padding: 50
+  },
+  goalInput:{
+    borderWidth: 2,
+    borderColor: 'green',
+    padding: 10,
+    marginBottom: 2,
   },
   text:{
-    margin: 16,
-    padding: 16,
-    borderWidth: 3,
-    borderColor: 'blue'
+    color: 'red'
   }
 });
