00d9a8d38b5a10b2377f4feab755d6189cfef3f6
Ishan Sharma <ishansharma1320@gmail.com>
2023-01-21 21:36:55 -0400

    Lecture 22 and 23: Handling Events and outputting list of goals on UI

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, TextInput, View, Button} from 'react-native';
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState('');
+  const [listOfGoals, setListOfGoals] = useState([]);
+  function handleTextInput(enteredText){
+    setEnteredText(enteredText);
+  }
+ 
+  function handleAddGoalButtonClick(){
+    setListOfGoals(currentGoals => [...listOfGoals,enteredText]);
+  }
+
   return (
     <View style={styles.appContainer}>
      <View style={styles.inputContainer}>
-      <TextInput style={styles.goalInput} placeholder='Ex: My Goal is to ...'></TextInput>
-      <Button title='Add Goal' />
+      <TextInput style={styles.goalInput} onChangeText={handleTextInput} placeholder='Ex: My Goal is to ...'></TextInput>
+      <Button title='Add Goal' onPress={handleAddGoalButtonClick}/>
      </View>
-     <View>
-      <Text style={styles.text}> List of Goals .... </Text>
+     <View style={styles.goalContainer}>
+      {listOfGoals.map((elem, index)=><Text style={styles.text} key={index}>{elem}</Text>)}
      </View>
     </View>
   );
@@ -17,22 +27,33 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer:{
-    padding: 50
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
+    
   },
   inputContainer:{
+    flex: 1,
     flexDirection: 'row',
     justifyContent:'space-between',
-   
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 24,
+    
   },
   goalInput:{
     borderWidth: 2,
     borderColor: 'green',
     padding: 10,
-    marginBottom: 2,
     width: '70%',
+    
     marginRight: 8
   },
   text:{
     color: 'red'
+  },
+  goalContainer:{
+    flex: 5
   }
 });
