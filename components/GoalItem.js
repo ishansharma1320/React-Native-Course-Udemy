fcd4ef02b8cb00d3a436a05d01f692ff74411627
Ishan Sharma <ishansharma1320@gmail.com>
2023-01-22 20:42:31 -0400

    Lecture 33: Platform specific styling

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -6,11 +6,13 @@ export default function GoalItem(props){
     function onDeleteOnTap(){
         props.onDeleteOnTap(props.id);
     }
-    return (<Pressable onPress={onDeleteOnTap}>
-        <View  style={styles.goalView}>
-        <Text style={styles.text} >{props.text}</Text>
-       </View>
-    </Pressable>);
+    return (
+        <View style={styles.goalView}>
+            <Pressable onPress={onDeleteOnTap} android_ripple={{backgroundColor: '#210644'}} style={({pressed})=>pressed && styles.pressedText}>
+                <Text style={styles.text} >{props.text}</Text>
+            </Pressable>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -20,10 +22,11 @@ const styles = StyleSheet.create({
         marginBottom: 3,
         alignItems: 'center'
       },
-    
+      pressedText:{
+        opacity: 0.5
+      },
       text:{
         color: 'white',
         padding: 10,
-        
       },
 })
